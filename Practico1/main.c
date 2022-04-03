8a0eabe5da00e8c1dbc3de498ad767974240f68f
Santiago Calvo <sncalvo5@gmail.com>
2022-04-03 16:21:00 -0300

    Add data

--- Practico1/main.c
+++ Practico1/main.c
@@ -61,25 +61,25 @@ int main(int argc, char* argv[]) {
 
    // BENCH_RUN(suma_est_fil(A_est), t_suma_est_fil, t_suma_est_fil_runs)
    // BENCH_RUN(suma_est_col(A_est), t_suma_est_col, t_suma_est_col_runs)
-   BENCH_RUN(suma_fil(A,n), t_suma_fil, t_suma_fil_runs)
-   BENCH_RUN(suma_col(A,n), t_suma_col, t_suma_col_runs)
-   BENCH_RUN(suma_rand(A,n), t_suma_rand, t_suma_rand_runs)
+   // BENCH_RUN(suma_fil(A,n), t_suma_fil, t_suma_fil_runs)
+   // BENCH_RUN(suma_col(A,n), t_suma_col, t_suma_col_runs)
+   // BENCH_RUN(suma_rand(A,n), t_suma_rand, t_suma_rand_runs)
 
    // printf("suma_est_fil: %.8f ms\truns:%d\n", t_suma_est_fil, t_suma_est_fil_runs );
    // printf("suma_est_col: %.8f ms\truns:%d\n", t_suma_est_col, t_suma_est_col_runs );
-   printf("suma_fila: %.8f ms\truns:%d\n", t_suma_fil, t_suma_fil_runs);
-   printf("suma_col: %.8f ms\truns:%d\n", t_suma_col, t_suma_col_runs);
-   printf("suma_rand: %.8f ms\truns:%d\n", t_suma_rand, t_suma_rand_runs);
+   // printf("suma_fila: %.8f ms\truns:%d\n", t_suma_fil, t_suma_fil_runs);
+   // printf("suma_col: %.8f ms\truns:%d\n", t_suma_col, t_suma_col_runs);
+   // printf("suma_rand: %.8f ms\truns:%d\n", t_suma_rand, t_suma_rand_runs);
 
-   // BENCH_RUN(mult_simple(A, B, C, n), t_mm_simple, t_mm_simple_runs)
-   // BENCH_RUN(mult_fila(A, B, C, n), t_mm_fila, t_mm_fila_runs)
-   // BENCH_RUN(mult_bl_simple(A, B, C, n, nb), t_mm_bl_simple, t_mm_bl_simple_runs)
-   // BENCH_RUN(mult_bl_fila(A, B, C, n, nb), t_mm_bl_fila, t_mm_bl_fila_runs)
+   BENCH_RUN(mult_simple(A, B, C, n), t_mm_simple, t_mm_simple_runs)
+   BENCH_RUN(mult_fila(A, B, C, n), t_mm_fila, t_mm_fila_runs)
+   BENCH_RUN(mult_bl_simple(A, B, C, n, nb), t_mm_bl_simple, t_mm_bl_simple_runs)
+   BENCH_RUN(mult_bl_fila(A, B, C, n, nb), t_mm_bl_fila, t_mm_bl_fila_runs)
 
-   // printf("mult_simple: %.2f ms, %.2f GFlops runs: %d\n", t_mm_simple, (((double)n / t_mm_simple ) * ((double)n / 1000.0)*((double)n / 1000.0)), t_mm_simple_runs);
-   // printf("mult_fila: %.2f ms, %.2f GFlops runs: %d\n", t_mm_fila, (((double)n / t_mm_fila) * ((double)n / 1000.0)*((double)n / 1000.0)), t_mm_fila_runs);
-   // printf("mult_bl_simple: %.2f ms, %.2f GFlops runs: %d\n", t_mm_bl_simple, (((double)n / t_mm_bl_simple)*((double)n / 1000.0) * ((double)n / 1000.0)), t_mm_bl_simple_runs);
-   // printf("mult_bl_fila: %.2f ms, %.2f GFlops runs: %d\n", t_mm_bl_fila, (((double)n / t_mm_bl_fila) * ((double)n / 1000.0) * ((double)n / 1000.0)), t_mm_bl_fila_runs );
+   printf("mult_simple: %.2f ms, %.2f GFlops runs: %d\n", t_mm_simple, (((double)n / t_mm_simple ) * ((double)n / 1000.0)*((double)n / 1000.0)), t_mm_simple_runs);
+   printf("mult_fila: %.2f ms, %.2f GFlops runs: %d\n", t_mm_fila, (((double)n / t_mm_fila) * ((double)n / 1000.0)*((double)n / 1000.0)), t_mm_fila_runs);
+   printf("mult_bl_simple: %.2f ms, %.2f GFlops runs: %d\n", t_mm_bl_simple, (((double)n / t_mm_bl_simple)*((double)n / 1000.0) * ((double)n / 1000.0)), t_mm_bl_simple_runs);
+   printf("mult_bl_fila: %.2f ms, %.2f GFlops runs: %d\n", t_mm_bl_fila, (((double)n / t_mm_bl_fila) * ((double)n / 1000.0) * ((double)n / 1000.0)), t_mm_bl_fila_runs );
 
 	return 0;
 }
@@ -174,8 +174,8 @@ int mult_fila(const VALT * __restrict__ A, const VALT * __restrict__ B, VALT * _
 int mult_bl_simple(const VALT * __restrict__ A, const VALT * __restrict__ B, VALT * __restrict__ C, size_t n, size_t bl_sz) {
    for (int bi = 0; bi < n; bi += bl_sz) {
       for (int bj = 0; bj < n; bj += bl_sz) {
-         for (int i = bi; i < bi + bl_sz; ++i) {
-            for (int j = bj; j < bj + bl_sz; ++j) {
+         for (int i = bi; i < bi + bl_sz && i < n; ++i) {
+            for (int j = bj; j < bj + bl_sz && j < n; ++j) {
                for (int k = 0; k < n; ++k) {
                   double temp = A[i * n + k] * B[k * n + j];
                   if (k != 0) {
@@ -194,8 +194,8 @@ int mult_bl_simple(const VALT * __restrict__ A, const VALT * __restrict__ B, VAL
 int mult_bl_fila(const VALT * __restrict__ A, const VALT * __restrict__ B, VALT * __restrict__ C, size_t n, size_t bl_sz) {
    for (int bi = 0; bi < n; bi += bl_sz) {
       for (int bj = 0; bj < n; bj += bl_sz) {
-         for (int i = bi; i < bi + bl_sz; ++i) {
-            for (int j = bj; j < bj + bl_sz; ++j) {
+         for (int i = bi; i < bi + bl_sz && i < n; ++i) {
+            for (int j = bj; j < bj + bl_sz && j < n; ++j) {
                for (int k = 0; k < n; ++k) {
                   double temp = A[i * n + j] * B[j * n + k];
                   if (j != 0) {
